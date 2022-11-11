9d3361fba78f27e5f20ae496f86d6b774c5743ba
henrikfjell <h1rik.f@gmail.com>
2022-11-11 09:24:02 +0100

    testing of kinematics seperately

--- gridMap/limo.py
+++ gridMap/limo.py
@@ -23,7 +23,7 @@ class LIMO:
         self.heading = self.get_heading(self.X[2:])
         self.v = np.sqrt(self.X[2]**2 + self.X[3]**2)
 
-    def kinematics(self, alpha, dt):
+    def one_step_alpha(self, alpha, dt):
         """ NB: Extending this code
                 - Take in (a, alpha) instead, and make |v| variable.
                 - Needs to keep a heading, and use it when |v| goes from zero to nonzero.
@@ -52,19 +52,57 @@ class LIMO:
 
         self.heading = self.get_heading(self.X[2:])
 
+    def kinematic_series(self, alpha, dt, steps):
+        # Store the trajectory
+        trajectory = np.zeros((4, 1, steps))
+        # Step 1: calculate turn rate
+        omega = self.v * np.tan(alpha)/self.length
+        # Step 2: update state vector
+        if omega != 0:
+            A = np.array([[1, 0, np.sin(omega*dt)/omega, -(1-np.cos(omega*dt))/omega],
+                          [0, 1, (1-np.cos(omega*dt))/omega, np.sin(omega*dt)/omega],
+                          [0, 0, np.cos(omega*dt), -np.sin(omega*dt)], 
+                          [0, 0, np.sin(omega*dt), np.cos(omega*dt)]], dtype=np.ndarray)
+        else: # In case omega == 0; straight line driving
+        # NB! If |v| was zero, need to find new heading?
+
+            A = np.array([[1, 0, dt, 0],
+                          [0, 1, 0, dt],
+                          [0, 0, 1, 0], 
+                          [0, 0, 0, 1]])
+        trajectory[:, :, 0] = self.X
+
+        # Step 3: run a number of updates
+        for i in range(steps):
+            self.X = A@self.X
+            trajectory[:, :, i] = self.X
+
+        return trajectory
+
     def get_heading(self, v):
         v_u = np.reshape((v / np.linalg.norm(v)), (2,))
         x_u = np.array([1, 0])
-
         return np.arccos(np.clip(np.dot(v_u, x_u), -1.0, 1.0))
 
 
 if __name__ == "__main__":
-    X = np.array([[0], [0], [0], [0.05]])
+    ### Heading test
+    test_heading=True
+    if test_heading==True:
+        X = np.array([[0], [0], [0], [0.05]])
+        heading_test_robot = LIMO(X)
+        print(np.rad2deg(heading_test_robot.heading))
+        print(heading_test_robot.X)
 
-    robot = LIMO(X)
-    print(np.rad2deg(robot.heading))
-    print(robot.X)
-    robot.kinematics(0.2, 10)
-    print(robot.X)
-    print(np.rad2deg(robot.heading))
+        X = np.array([[0], [0], [0.5], [0.5]])
+        heading_test_robot = LIMO(X)
+        print(np.rad2deg(heading_test_robot.heading))
+        print(heading_test_robot.X)
+
+        X = np.array([[0], [0], [0.5], [0]])
+        heading_test_robot = LIMO(X)
+        print(np.rad2deg(heading_test_robot.heading))
+        print(heading_test_robot.X)
+    
+    X = np.array([[0], [0], [0.5], [0.5]])
+    robot = LIMO()
