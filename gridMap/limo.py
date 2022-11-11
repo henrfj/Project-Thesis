f8c0cef4d9de8734ef8930369d0029662289d637
henrfj <henrfj@stud.ntnu.no>
2022-11-11 17:08:10 +0100

    new improved kinematics implemented in limo

--- gridMap/limo.py
+++ gridMap/limo.py
@@ -6,103 +6,71 @@ def distance(point1, point2):
 
 
 class LIMO:
-    """ A general Ackerman driven robot platform """
+    """ A general Ackerman driven robot platform.
+     Modelled using simple coordinated turm motion. """
     
-    def __init__(self, start_state=np.array([[0], [0], [0], [0]])) -> None:
+    def __init__(self, dt, gamma) -> None:
         # Intrinsics
-        self.m2p = 4000 # Pixels per meter
         self.maxspeed = 2 # m/s
-        self.length = 0.5
-        self.width = 0.2 # Width in meters
+        self.max_wheel_angle = 1.2 # About 66 degrees
+        self.d = 0.5 # Distance between front and back axels
 
         # Extrinsics
-        self.min_obs_dist = 100 # pixels
-        self.count_down = 5 # seconds
-        #self.dt = dt # Time between discrete simulations
-        self.X = start_state
-        self.heading = self.get_heading(self.X[2:])
-        self.v = np.sqrt(self.X[2]**2 + self.X[3]**2)
+        self.X = np.array([[0], [0], [0], [0]])
+        self.psi = 0    # Heading
+        self.alpha = 0  # Wheel angle. Throttle level can be derived from X[2:,:]
+        self.omega = 0  # Current turning rate
 
-    def one_step_alpha(self, alpha, dt):
-        """ NB: Extending this code
-                - Take in (a, alpha) instead, and make |v| variable.
-                - Needs to keep a heading, and use it when |v| goes from zero to nonzero.
-            Also:
-                - Make code more efficient by not having to recalculate omega, A for repeating value (v, alpha)
+        # Tunable parameters
+        self.K_a = 0.1 # Wheel angle update rate
+        self.K_v = 0.1 # Wheel 
+        self.dt = dt # Time between discrete simulations
+        self.gamma = gamma
+
+    def one_step_algorithm(self, alpha_ref, v_ref):
         """
-        # Step 1: calculate turn rate
-        omega = self.v * np.tan(alpha)/self.length
-
-        # Step 2: update state vector
-        if omega != 0:
-            A = np.array([[1, 0, np.sin(omega*dt)/omega, -(1-np.cos(omega*dt))/omega],
-                          [0, 1, (1-np.cos(omega*dt))/omega, np.sin(omega*dt)/omega],
-                          [0, 0, np.cos(omega*dt), -np.sin(omega*dt)], 
-                          [0, 0, np.sin(omega*dt), np.cos(omega*dt)]], dtype=np.ndarray)
-        else: # In case omega == 0; straight line driving
-        # NB! If |v| was zero, need to find new heading?
-
-            A = np.array([[1, 0, dt, 0],
-                          [0, 1, 0, dt],
+        Running one step of the update algorithm.
+        """
+        # Step 1: update u, alpha, find B_k
+        self.alpha = self.alpha + self.K_a * (alpha_ref - self.alpha)
+        v_k = self.X[2:,:] # Current speed from state matrix
+        u_k = self.K_v * (np.linalg.norm(v_ref)-np.linalg.norm(v_k))
+        B_k = np.array([[0],
+                        [0], 
+                        [np.cos(self.psi)], 
+                        [np.sin(self.psi)]])
+    
+        # Step 2: Choose A_k
+        if np.abs(self.omega) > self.gamma:
+            A_k = np.array([[1, 0, np.sin(self.omega*self.dt)/self.omega, -(1-np.cos(self.omega*self.dt))/self.omega],
+                          [0, 1, (1-np.cos(self.omega*self.dt))/self.omega, np.sin(self.omega*self.dt)/self.omega],
+                          [0, 0, np.cos(self.omega*self.dt), -np.sin(self.omega*self.dt)], 
+                          [0, 0, np.sin(self.omega*self.dt), np.cos(self.omega*self.dt)]], dtype=np.ndarray)
+        else: # In case omega ~ 0
+            A_k = np.array([[1, 0, self.dt, 0],
+                          [0, 1, 0, self.dt],
                           [0, 0, 1, 0], 
                           [0, 0, 0, 1]])
         
-        # Step 3: run one step
-        self.X = A@self.X
+        # Step 3: Update state matrix
+        self.X = A_k@self.X + B_k*u_k
 
-        self.heading = self.get_heading(self.X[2:])
-
-    def kinematic_series(self, alpha, dt, steps):
-        # Store the trajectory
-        trajectory = np.zeros((4, 1, steps))
-        # Step 1: calculate turn rate
-        omega = self.v * np.tan(alpha)/self.length
-        # Step 2: update state vector
-        if omega != 0:
-            A = np.array([[1, 0, np.sin(omega*dt)/omega, -(1-np.cos(omega*dt))/omega],
-                          [0, 1, (1-np.cos(omega*dt))/omega, np.sin(omega*dt)/omega],
-                          [0, 0, np.cos(omega*dt), -np.sin(omega*dt)], 
-                          [0, 0, np.sin(omega*dt), np.cos(omega*dt)]], dtype=np.ndarray)
-        else: # In case omega == 0; straight line driving
-        # NB! If |v| was zero, need to find new heading?
-
-            A = np.array([[1, 0, dt, 0],
-                          [0, 1, 0, dt],
-                          [0, 0, 1, 0], 
-                          [0, 0, 0, 1]])
-        trajectory[:, :, 0] = self.X
-
-        # Step 3: run a number of updates
-        for i in range(steps):
-            self.X = A@self.X
-            trajectory[:, :, i] = self.X
-
-        return trajectory
-
-    def get_heading(self, v):
-        v_u = np.reshape((v / np.linalg.norm(v)), (2,))
-        x_u = np.array([1, 0])
-        return np.arccos(np.clip(np.dot(v_u, x_u), -1.0, 1.0))
+        # Step 4: Update omega nad psi (based on theta)
+        theta_next = np.linalg.norm(v_k)*self.dt*np.tan(self.alpha) / self.d # Turn angle
+        self.psi = self.psi + theta_next # Heading in WC
+        self.omega = np.linalg.norm(v_k)*np.tan(self.alpha) / self.d
 
 
 if __name__ == "__main__":
-    ### Heading test
-    test_heading=True
-    if test_heading==True:
-        X = np.array([[0], [0], [0], [0.05]])
-        heading_test_robot = LIMO(X)
-        print(np.rad2deg(heading_test_robot.heading))
-        print(heading_test_robot.X)
+    ### Testing single steps
+    robot = LIMO(dt=0.1, gamma=5e-7)
+    steps = 1000
+    v_ref = 1
+    alpha_ref = 0
+    states = np.zeros((4, 1, steps))
+    alphas = np.zeros((steps,))
 
-        X = np.array([[0], [0], [0.5], [0.5]])
-        heading_test_robot = LIMO(X)
-        print(np.rad2deg(heading_test_robot.heading))
-        print(heading_test_robot.X)
-
-        X = np.array([[0], [0], [0.5], [0]])
-        heading_test_robot = LIMO(X)
-        print(np.rad2deg(heading_test_robot.heading))
-        print(heading_test_robot.X)
-    
-    X = np.array([[0], [0], [0.5], [0.5]])
-    robot = LIMO()
+    for i in range(100):
+        alphas[i] = robot.alpha
+        states[:, :, i] = robot.X
+        robot.one_step_algorithm(alpha_ref=alpha_ref, v_ref=v_ref)
